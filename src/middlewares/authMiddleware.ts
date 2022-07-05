2065ae5a5ea5739d629e0d088531486940ec5c15
myguel_angello <myguelangello624@gmail.com>
2022-07-05 00:49:59 -0300

    changed the room code to a random number generator

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 import { verify } from 'jsonwebtoken';
 
 type TokenPayload = {
-  id: string;
+  id?: string;
   iat: number;
   exp: number;
 }
